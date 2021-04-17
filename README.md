2667c79555591de052c28055a81e8742d66e8bbb
yuchanns <airamusume@gmail.com>
2021-04-17 19:58:36 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hugo Theme Yuchanns
+
+The CSS Style is Inspired From [Himself65](https://github.com/Himself65/himself65.github.io).
+
+Preview: [TENCENT CDN](https://www.yuchanns.xyz).
---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+MathJax = {
+    tex: {
+        inlineMath: [['$', '$']]
+    },
+    svg: {
+        fontCache: 'global'
+    }
+};
+
+window.MathJax.Hub.Queue(['Typeset', window.MathJax.Hub])
+
+var prefer_dark = window.matchMedia("(prefers-color-scheme: dark)").matches
+
+window.onload = function () {
+    if (prefer_dark) {
+        window.document.body.classList.replace("light", "dark")
+    } else {
+        window.document.body.classList.replace("dark", "light")
+    }
+}
+
+function toggle_theme() {
+    if (window.document.body.classList.contains("light")) {
+        window.document.body.classList.replace("light", "dark")
+    } else {
+        window.document.body.classList.replace("dark", "light")
+    }
+}
