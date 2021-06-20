3295ed4a4af71702126952aa53b73434dbd212ff
yuchanns <airamusume@gmail.com>
2021-06-21 01:08:10 +0800

    remove manual toggle theme button

--- static/js/main.js
+++ static/js/main.js
@@ -6,28 +6,3 @@ MathJax = {
         fontCache: 'global'
     }
 };
-
-document.addEventListener("DOMContentLoaded", function () {
-    const prefer_dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const mode = localStorage.getItem('mode');
-    if (!mode && prefer_dark) {
-        window.document.body.classList.replace("light", "dark")
-    } else if (mode) {
-        const old = mode === "dark" ? "light" : "dark"
-        window.document.body.classList.replace(old, mode)
-    }
-})
-
-function toggle_theme() {
-    let prefer, old;
-    if (window.document.body.classList.contains("light")) {
-        prefer = "dark"
-        old = "light"
-
-    } else {
-        prefer = "light"
-        old = "dark"
-    }
-    window.document.body.classList.replace(old, prefer)
-    localStorage.setItem("mode", prefer)
-}
